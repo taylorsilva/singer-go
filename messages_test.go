9ad92b1f4119ec678ac78fe554d91bdd896e14e5
taylor <taylorsilva@outlook.com>
2018-03-05 22:11:31 -0500

    Add test for WriteSchema

--- messages_test.go
+++ messages_test.go
@@ -1,18 +1,18 @@
 package singer
 
 import (
+	"bytes"
 	"testing"
 	"time"
-	"bytes"
 )
 
 var (
 	testLocation, _ = time.LoadLocation("America/New_York")
-	testDateTime = time.Date(2000, time.January, 1,0,0,0,0, testLocation)
+	testDateTime    = time.Date(2000, time.January, 1, 0, 0, 0, 0, testLocation)
+	writer          = new(bytes.Buffer)
 )
 
 func TestWriteRecord(t *testing.T) {
-	var writer = new(bytes.Buffer)
 	OUTPUT = writer
 	result := string(`{"type":"RECORD","stream":"streamValue","record":{"name":"foo"},"version":"","time_extracted":"2000-01-01T00:00:00-05:00"}
 `)
@@ -20,6 +20,17 @@ func TestWriteRecord(t *testing.T) {
 	if writer.String() != result {
 		t.Error("Expected: ", result,
 			"Got: ", writer.String())
+	}
+	writer.Reset()
+}
+
+func TestWriteSchema(t *testing.T) {
+	OUTPUT = writer
+	result := string(`{"type":"SCHEMA","stream":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"},"key_properties":["name"]}
+`)
+	WriteSchema("users", []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`), []string {"name"})
+	if writer.String() != result {
+		t.Error("Expected:", result, "Got:", writer.String())
 	}
 	writer.Reset()
 }
