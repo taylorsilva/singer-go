14c10ab49273ec6946f117c7d7f4bff342fd4048
taylor <taylorsilva@outlook.com>
2018-03-06 22:40:35 -0500

    Write test for testing basic record outputting

--- messages_test.go
+++ messages_test.go
@@ -13,21 +13,25 @@ var (
 func TestWriteRecord(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
-	result := string(`{"type":"RECORD","stream":"streamValue","record":{"id":12,"name":"foo"}}`)
+	result := []byte(`{"type":"RECORD","stream":"streamValue","record":{"id":12,"name":"foo"}}`)
 	WriteRecord("streamValue", []byte(`{"id":12,"name": "foo"}`))
 	g.Expect(writer.String()).To(gomega.MatchJSON(result), "Single record should match")
 	writer.Reset()
 }
 
 func TestWriteRecords(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
-	result := string(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}
-{"type":"RECORD","stream":"users","record":{"id":2,"name":"Mike"}}
-`)
+	result := [][]byte {[]byte(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}`),
+		[]byte(`{"type":"RECORD","stream":"users","record":{"id":2,"name":"Mike"}}`)}
 	records := [][]byte {[]byte(`{"id":1,"name":"Chris"}`),[]byte(`{"id":2,"name":"Mike"}`)}
 	WriteRecords("users", records)
-	if writer.String() != result {
-		t.Error("Expected:", result, "Got:", writer.String())
+	for i := 0; ;i++ {
+		line, err := writer.ReadBytes(byte('\n'))
+		if err != nil {
+			break
+		}
+		g.Expect(line).To(gomega.MatchJSON(result[i]), "Each record should output on its own line")
 	}
 	writer.Reset()
 }
