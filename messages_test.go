0fe01480f83185ddc4826b5e61c416a687db0686
taylor <taylorsilva@outlook.com>
2018-03-10 13:49:09 -0500

    Test for generating schema json object to output

--- messages_test.go
+++ messages_test.go
@@ -2,12 +2,12 @@ package singer
 
 import (
 	"bytes"
-	"testing"
 	"github.com/onsi/gomega"
+	"testing"
 )
 
 var (
-	writer          = new(bytes.Buffer)
+	writer = new(bytes.Buffer)
 )
 
 func TestWriteRecord(t *testing.T) {
@@ -15,18 +15,18 @@ func TestWriteRecord(t *testing.T) {
 	OUTPUT = writer
 	result := []byte(`{"type":"RECORD","stream":"streamValue","record":{"id":12,"name":"foo"}}`)
 	WriteRecord("streamValue", []byte(`{"id":12,"name": "foo"}`))
-	g.Expect(writer.String()).To(gomega.MatchJSON(result), "Single record should match")
+	g.Expect(writer.String()).To(gomega.MatchJSON(result), "JSON records should match")
 	writer.Reset()
 }
 
 func TestWriteRecords(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
-	result := [][]byte {[]byte(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}`),
+	result := [][]byte{[]byte(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}`),
 		[]byte(`{"type":"RECORD","stream":"users","record":{"id":2,"name":"Mike"}}`)}
-	records := [][]byte {[]byte(`{"id":1,"name":"Chris"}`),[]byte(`{"id":2,"name":"Mike"}`)}
+	records := [][]byte{[]byte(`{"id":1,"name":"Chris"}`), []byte(`{"id":2,"name":"Mike"}`)}
 	WriteRecords("users", records)
-	for i := 0; ;i++ {
+	for i := 0; ; i++ {
 		line, err := writer.ReadBytes(byte('\n'))
 		if err != nil {
 			break
@@ -37,12 +37,15 @@ func TestWriteRecords(t *testing.T) {
 }
 
 func TestWriteSchema(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
-	result := string(`{"type":"SCHEMA","stream":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"},"key_properties":["name"]}
+	result := []byte(`{"type":"SCHEMA","streamName":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"},"key_properties":["name"]}
 `)
-	WriteSchema("users", []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`), []string {"name"}, []string{})
-	if writer.String() != result {
-		t.Error("Expected:", result, "Got:", writer.String())
-	}
+	streamName := "users"
+	schema := []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`)
+	keyProperties := []string{"name"}
+	bookmarks := []string{}
+	WriteSchema(streamName, schema, keyProperties, bookmarks)
+	g.Expect(writer.String()).To(gomega.MatchJSON(result), "Output should be Schema JSON object")
 	writer.Reset()
 }
