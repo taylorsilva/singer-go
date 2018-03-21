fb2643f8643bd5d95004f4e57b613d5f2f03eb0c
taylor <taylorsilva@outlook.com>
2018-03-20 22:46:50 -0400

    write test for parsing messages

--- messages_test.go
+++ messages_test.go
@@ -2,9 +2,10 @@ package singer
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/onsi/gomega"
 	"testing"
-	"encoding/json"
+	"time"
 )
 
 var (
@@ -15,6 +16,7 @@ func TestWriteRecord(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
 	defer writer.Reset()
+
 	result := []byte(`{"type":"RECORD","stream":"streamValue","record":{"id":12,"name":"foo"}}`)
 	WriteRecord("streamValue", []byte(`{"id":12,"name": "foo"}`))
 	g.Expect(writer.String()).To(gomega.MatchJSON(result), "JSON records should match")
@@ -24,9 +26,14 @@ func TestWriteRecords(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
 	defer writer.Reset()
-	result := [][]byte{[]byte(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}`),
-		[]byte(`{"type":"RECORD","stream":"users","record":{"id":2,"name":"Mike"}}`)}
+
+	result := [][]byte{
+		[]byte(`{"type":"RECORD","stream":"users","record":{"id":1,"name":"Chris"}}`),
+		[]byte(`{"type":"RECORD","stream":"users","record":{"id":2,"name":"Mike"}}`),
+	}
+
 	records := [][]byte{[]byte(`{"id":1,"name":"Chris"}`), []byte(`{"id":2,"name":"Mike"}`)}
+
 	WriteRecords("users", records)
 	for i := 0; ; i++ {
 		line, err := writer.ReadBytes(byte('\n'))
@@ -41,12 +48,15 @@ func TestWriteSchema(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
 	defer writer.Reset()
+
 	result := []byte(`{"type":"SCHEMA","stream":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"},"key_properties":["name"]}`)
+
 	streamName := "users"
 	schema := []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`)
 	keyProperties := []string{"name"}
 	WriteSchema(streamName, schema, keyProperties)
 	g.Expect(writer.String()).To(gomega.MatchJSON(result), "Output should be Schema JSON object")
+
 	lastByte := writer.Bytes()[len(writer.Bytes())-1]
 	g.Expect(lastByte).To(gomega.Equal(byte('\n')), "Output should end in line break")
 }
@@ -55,13 +65,16 @@ func TestWriteSchemaExtras(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
 	defer writer.Reset()
+
 	result := []byte(`{"type":"SCHEMA","stream":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"},"key_properties":["name"], "bookmark_properties":["name"]}`)
+
 	streamName := "users"
 	schema := []byte(`{"type": "object", "properties": {"name": {"type": "string"}}}`)
 	keyProperties := []string{"name"}
 	bookmarks := []string{"name"}
 	WriteSchemaExtras(streamName, schema, keyProperties, bookmarks)
 	g.Expect(writer.String()).To(gomega.MatchJSON(result), "Output should be Schema JSON object")
+
 	lastByte := writer.Bytes()[len(writer.Bytes())-1]
 	g.Expect(lastByte).To(gomega.Equal(byte('\n')))
 }
@@ -70,9 +83,10 @@ func TestWriteState(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	OUTPUT = writer
 	defer writer.Reset()
+
 	result := []byte(`{"type": "STATE", "value": {"users": 2, "locations": 1}}`)
 	value := map[string]interface{}{
-		"users": 2,
+		"users":     2,
 		"locations": 1,
 	}
 	j, _ := json.Marshal(value)
@@ -81,4 +95,55 @@ func TestWriteState(t *testing.T) {
 
 	lastByte := writer.Bytes()[len(writer.Bytes())-1]
 	g.Expect(lastByte).To(gomega.Equal(byte('\n')), "Output should end in line break")
+}
+
+func TestParseMessage(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	tests := []struct {
+		input  []byte
+		output Message
+		err    error
+	}{
+		{
+			input:  []byte(`{"type": "STATE", "value": {"users": 2, "locations": 1}}`),
+			output: stateMessage{Type: "STATE", Value: map[string]interface{}{"users": 2, "locations": 1}},
+			err:    nil,
+		},
+		{
+			input: []byte(`{"type":"SCHEMA","stream":"users","schema":{"properties":{"name":{"type":"string"}},"type":"object"}}`),
+			output: schemaMessage{
+				Type:   "SCHEMA",
+				Stream: "users",
+				Schema: map[string]interface{}{
+					"properties": map[string]interface{}{
+						"name": map[string]interface{}{
+							"type": "string",
+						},
+						"type": "object",
+					},
+				}},
+			err: nil,
+		},
+		{
+			input: []byte(`{"type":"RECORD","stream":"streamValue","record":{"id":12,"name":"foo"}}`),
+			output: recordMessage{
+				Type:          "RECORD",
+				Stream:        "streamValue",
+				Version:       "",
+				TimeExtracted: time.Time{},
+				Record: map[string]interface{}{
+					"id":   12,
+					"name": "foo",
+				},
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		msg, err := ParseMessage(test.input)
+		g.Expect(msg).To(gomega.Equal(test.output))
+		g.Expect(err).To(gomega.Equal(test.err))
+	}
 }
